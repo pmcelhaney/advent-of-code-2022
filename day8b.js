28c3dbee8c1c66991233126c1c5d7e80367aea22
Patrick McElhaney <pmcelhaney@gmail.com>
2022-12-08 15:18:54 -0500

    8b using reduce

--- day8b.js
+++ day8b.js
@@ -3,28 +3,26 @@ import { readInput } from "./reader.js";
 const grid = [];
 
 function countVisibleTrees(height, heights) {
-  const count = heights.findIndex((x) => x >= height);
+  const index = heights.findIndex((x) => x >= height);
 
-  return count === -1 ? heights.length : count + 1;
+  return index === -1 ? heights.length : index + 1;
 }
 
 function maxScenicScore() {
-  let max = 0;
+  return grid.reduce(
+    (accumulator, row, y) =>
+      row.reduce((accumlator2, tree, x) => {
+        const column = grid.map((c) => c[x]);
 
-  grid.forEach((row, y) => {
-    row.forEach((tree, x) => {
-      const column = grid.map((c) => c[x]);
+        const left = countVisibleTrees(tree, row.slice(0, x).reverse());
+        const right = countVisibleTrees(tree, row.slice(x + 1));
+        const up = countVisibleTrees(tree, column.slice(0, y).reverse());
+        const down = countVisibleTrees(tree, column.slice(y + 1));
 
-      const left = countVisibleTrees(tree, row.slice(0, x).reverse());
-      const right = countVisibleTrees(tree, row.slice(x + 1));
-      const up = countVisibleTrees(tree, column.slice(0, y).reverse());
-      const down = countVisibleTrees(tree, column.slice(y + 1));
-
-      max = Math.max(max, left * right * up * down);
-    });
-  });
-
-  return max;
+        return Math.max(accumlator2, left * right * up * down);
+      }, accumulator),
+    0
+  );
 }
 
 readInput(
